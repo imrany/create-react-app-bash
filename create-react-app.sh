1a0091ce2008323778d24d9088eddffdaddff314
imran matano <imranmat254@gmail.com>
2023-03-07 10:45:52 +0300

    Update create-react-app.sh

--- create-react-app.sh
+++ create-react-app.sh
@@ -3,6 +3,7 @@
 #ask the user to enter the folder name
 #create a react app with vite
 #cd into that folder
+#runs npm install
 #runs npm start
 
 read -p "Enter the name of the folder you would like to create a react app into: " FOLDER
